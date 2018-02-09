87a1ef2116643d088ac4a583e09181a3f4d6300c
Peter Hunt <peter.hunt@wework.com>
2018-02-09 21:23:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, index)
+  if array[index] == "   " || array[index] == "  " || array[index] == " " || array[index] == ""
+    false 
+  elsif array[index] == "X" || array[index] == "O" 
+    true
+  else array[index] == nil
+    false
+  end
+end 
